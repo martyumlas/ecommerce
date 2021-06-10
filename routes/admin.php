e80efdba67545aa385f4e66d170f0ba34cf68dd8
Marty Umlas <martyumlas@gmail.com>
2021-06-10 12:31:54 +0800

    categories views

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Admin\SettingController;
+use App\Http\Controllers\Admin\CategoryController;
 
 Route::group(['prefix'  =>  'admin'], function () {
 
@@ -18,6 +19,15 @@ Route::group(['prefix'  =>  'admin'], function () {
         
         Route::get('/settings', [SettingController::class, 'index'])->name('admin.settings');
         Route::post('/settings', [SettingController::class, 'update'])->name('admin.settings.update');
+
+        Route::group(['prefix'  =>   'categories'], function() {
+            Route::get('/', [CategoryController::class, 'index'])->name('admin.categories.index');
+            Route::get('/create', [CategoryController::class, 'create'])->name('admin.categories.create');
+            Route::post('/store', [CategoryController::class, 'store'])->name('admin.categories.store');
+            Route::get('/{id}/edit', [CategoryController::class, 'edit'])->name('admin.categories.edit');
+            Route::post('/update', [CategoryController::class, 'update'])->name('admin.categories.update');
+            Route::get('/{id}/delete', [CategoryController::class, 'delete'])->name('admin.categories.delete');        
+        });
     });
 
 });
