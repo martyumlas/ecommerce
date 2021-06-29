dd000a0d73be897b6bf761f3d99b06f5e6fbcc0f
Marty Umlas <martyumlas@gmail.com>
2021-06-29 11:14:46 +0800

    Product and Product Images model and migration

---
+++ app/Models/Product.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+     /**
+     * @var string
+     */
+    protected $table = 'products';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'brand_id', 'sku', 'name', 'slug', 'description', 'quantity',
+        'weight', 'price', 'sale_price', 'status', 'featured',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'quantity'  =>  'integer',
+        'brand_id'  =>  'integer',
+        'status'    =>  'boolean',
+        'featured'  =>  'boolean'
+    ];
+
+     /**
+     * @param $value
+     */
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+}
