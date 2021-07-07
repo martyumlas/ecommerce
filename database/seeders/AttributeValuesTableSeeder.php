223f147931e8bfa5907477559fbfac6aeea964a2
Marty Umlas <martyumlas@gmail.com>
2021-07-07 14:46:09 +0800

    update attributes and attributes values  seeder

--- database/seeders/AttributeValuesTableSeeder.php
+++ database/seeders/AttributeValuesTableSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Models\AttributeValue;
+use App\Models\AttributeValue;
 
 class AttributeValuesTableSeeder extends Seeder
 {
